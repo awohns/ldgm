432c0d5ccbaff2f1f4db73424245bce7b3d99f63
awohns <awohns@gmail.com>
2021-07-21 16:32:19 -0400

    toggle rule 2 in creating bricks graph

--- ld_graph/bricks_graph.py
+++ ld_graph/bricks_graph.py
@@ -12,11 +12,10 @@ from . import utility
 
 
 class BrickGraph:
-    def __init__(
-        self,
-        bricked_ts,
-    ):
+    def __init__(self, bricked_ts, use_rule_two=True):
         self.bricked_ts = bricked_ts
+        self.from_to_set = set()
+        self.use_rule_two = use_rule_two
 
     # make an argument for in and out here, so we know how to split if it's labeled
     def up_vertex(self, edge_id, in_out):
@@ -63,87 +62,93 @@ class BrickGraph:
         else:
             return 6 * edge_id + 3
 
-    def make_connections(
-        self, edge, tree2, node_edge_dict, from_to_set, index, prev_edge_dict
-    ):
-        # Rule 1: connect parent brick to its child bricks
-        for child in tree2.children(edge.child):
+    def rule_one(self, edge, children, node_edge_dict, roots):
+        # Rule 1: connect parent brick to its child bricks, then to its parent
+        for child in children:
             if node_edge_dict[child] != node_edge_dict[edge.child]:
-                from_to_set.add(
+                self.from_to_set.add(
                     (
                         self.up_vertex(node_edge_dict[child], "in"),
                         self.up_vertex(node_edge_dict[edge.child], "out"),
                     )
                 )
-                from_to_set.add(
+                self.from_to_set.add(
                     (
                         self.down_vertex(node_edge_dict[edge.child], "in"),
                         self.down_vertex(node_edge_dict[child], "out"),
                     )
                 )
-        roots = tree2.roots
         if edge.parent not in roots and edge.child not in roots:
             if node_edge_dict[edge.child] != node_edge_dict[edge.parent]:
-                from_to_set.add(
+                self.from_to_set.add(
                     (
                         self.up_vertex(node_edge_dict[edge.child], "in"),
                         self.up_vertex(node_edge_dict[edge.parent], "out"),
                     )
                 )
-                from_to_set.add(
+                self.from_to_set.add(
                     (
                         self.down_vertex(node_edge_dict[edge.parent], "in"),
                         self.down_vertex(node_edge_dict[edge.child], "out"),
                     )
                 )
+
+    def rule_two(self, edge, children, node_edge_dict):
         # Rule 2: Connect sibling bricks to one another
-        children = tree2.children(edge.parent)
         if len(children) > 1:
             if len(children) > 1:
                 for item in itertools.combinations(children, 2):
-                    from_to_set.add(
+                    self.from_to_set.add(
                         (
                             self.up_vertex(node_edge_dict[item[0]], "in"),
                             self.down_vertex(node_edge_dict[item[1]], "out"),
                         )
                     )
-                    from_to_set.add(
+                    self.from_to_set.add(
                         (
                             self.up_vertex(node_edge_dict[item[1]], "in"),
                             self.down_vertex(node_edge_dict[item[0]], "out"),
                         )
                     )
 
+    def rule_three(self, edge, children, node_edge_dict, prev_edge_dict):
         # Rule 3: Connect parent bricks sharing child haplotype across a recombination
+        if edge.child in prev_edge_dict:
+            # r,d of left parent to r of right parent
+            self.from_to_set.add(
+                (
+                    self.right_vertex(prev_edge_dict[edge.child], "in"),
+                    self.right_vertex(node_edge_dict[edge.child], "out"),
+                )
+            )
+            self.from_to_set.add(
+                (
+                    self.down_vertex(prev_edge_dict[edge.child], "in"),
+                    self.right_vertex(node_edge_dict[edge.child], "out"),
+                )
+            )
+            # l,d of right parent to l of left parent
+            self.from_to_set.add(
+                (
+                    self.left_vertex(node_edge_dict[edge.child], "in"),
+                    self.left_vertex(prev_edge_dict[edge.child], "out"),
+                )
+            )
+            self.from_to_set.add(
+                (
+                    self.down_vertex(node_edge_dict[edge.child], "in"),
+                    self.left_vertex(prev_edge_dict[edge.child], "out"),
+                )
+            )
+
+    def make_connections(self, edge, tree2, node_edge_dict, index, prev_edge_dict):
+        children = tree2.children(edge.parent)
+        roots = tree2.roots
+        self.rule_one(edge, children, node_edge_dict, roots)
+        if self.use_rule_two:
+            self.rule_two(edge, children, node_edge_dict)
         if index != 0:
-            if edge.child in prev_edge_dict:
-                # r,d of left parent to r of right parent
-                from_to_set.add(
-                    (
-                        self.right_vertex(prev_edge_dict[edge.child], "in"),
-                        self.right_vertex(node_edge_dict[edge.child], "out"),
-                    )
-                )
-                from_to_set.add(
-                    (
-                        self.down_vertex(prev_edge_dict[edge.child], "in"),
-                        self.right_vertex(node_edge_dict[edge.child], "out"),
-                    )
-                )
-                # l,d of right parent to l of left parent
-                from_to_set.add(
-                    (
-                        self.left_vertex(node_edge_dict[edge.child], "in"),
-                        self.left_vertex(prev_edge_dict[edge.child], "out"),
-                    )
-                )
-                from_to_set.add(
-                    (
-                        self.down_vertex(node_edge_dict[edge.child], "in"),
-                        self.left_vertex(prev_edge_dict[edge.child], "out"),
-                    )
-                )
-        return from_to_set
+            self.rule_three(edge, children, node_edge_dict, prev_edge_dict)
 
     def make_brick_graph(self):
         """
@@ -161,14 +166,13 @@ class BrickGraph:
         self.l_in = []
         self.l_out = []
 
-        from_to_set = set()
         node_edge_dict = {}
 
         # Rule Zero
         # For unlabeled nodes: connect left to up, right to up (within a brick)
         for brick in self.unlabeled_bricks:
-            from_to_set.add((6 * brick + 2, 6 * brick))
-            from_to_set.add((6 * brick + 3, 6 * brick))
+            self.from_to_set.add((6 * brick + 2, 6 * brick))
+            self.from_to_set.add((6 * brick + 3, 6 * brick))
 
         for index, (tree2, (_, edges_out, edges_in)) in tqdm(
             enumerate(zip(self.bricked_ts.trees(), self.bricked_ts.edge_diffs())),
@@ -183,19 +187,18 @@ class BrickGraph:
                 node_edge_dict[edge.child] = edge.id
 
             for edge in edges_in:
-                from_to_set = self.make_connections(
+                self.make_connections(
                     edge,
                     tree2,
                     node_edge_dict,
-                    from_to_set,
                     index,
                     prev_edge_dict,
                 )
 
         # Create networkx graph
         df = pd.Datamuts_to_merge_dict = {
-            "from": [cur_set[0] for cur_set in from_to_set],
-            "to": [cur_set[1] for cur_set in from_to_set],
+            "from": [cur_set[0] for cur_set in self.from_to_set],
+            "to": [cur_set[1] for cur_set in self.from_to_set],
         }
         self.G = nx.from_pandas_edgelist(df, "from", "to", create_using=nx.DiGraph())
         self.labeled_nodes = np.unique(self.labeled_nodes)
