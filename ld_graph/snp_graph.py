9889e4082452ab1c65d84ac1d4dc1fec0d1334a9
awohns <awohns@gmail.com>
2021-07-21 17:50:32 -0400

    added new regularization code

--- ld_graph/snp_graph.py
+++ ld_graph/snp_graph.py
@@ -1,19 +1,16 @@
 """
 Create a reduced SNP graph
 """
-import collections
-
 import networkx as nx
 import numpy as np
-from tqdm import tqdm
 
 from . import utility
 
 
 class SNP_Graph:
-    def __init__(self, brick_graph, brick_ts):
+    def __init__(self, brick_graph, brick_ts, identify_in_out=False):
         self.brick_graph = brick_graph
-        self.brick_ts = brick_ts
+        self.identify_in_out = identify_in_out
 
         # Tree sequence must contain mutations
         if brick_ts.num_mutations == 0:
@@ -38,35 +35,51 @@ class SNP_Graph:
         self.bricks_to_id = bricks_to_id
         self.id_to_muts = id_to_muts
 
-        nodes = np.array(list(brick_graph.nodes()))
-        self.l_out = nodes[nodes % 6 == 4]
-        self.l_in = nodes[nodes % 6 == 5]
-
     def create_reduced_graph(self):
-        desc = collections.defaultdict()
-        R_desc = nx.Graph()
-        R_desc.add_nodes_from(self.l_out)
-        R_desc.add_nodes_from(self.l_in)
-        for u in tqdm(self.l_out, total=len(self.l_out), desc="Graph reduction"):
-            desc[u] = nx.descendants(self.brick_graph, u)
-            for v in desc[u]:
-                if v in self.l_in:
-                    R_desc.add_edge(u, v)
-        nodes = list(R_desc.nodes())
-        equivalent_nodes = []
-        vals = np.stack([self.l_out, self.l_out + 1]).transpose()
-        for index, i in tqdm(enumerate(vals)):
-            if i[0] in nodes and i[1] in nodes:
-                equivalent_nodes.append(vals[index])
-        R_identified = nx.quotient_graph(R_desc, equivalent_nodes)
+        print("Condensing graph...")
+        C = nx.condensation(self.brick_graph)
+        print("Finding transitive closure...")
+        H = nx.transitive_closure_dag(C, topo_order=None)
 
-        def node_data(list_of_lists):
-            node_data = {}
-            for _, i in enumerate(list_of_lists):
-                node_data[frozenset({i[0], i[1]})] = int(i[0] / 6)
-            return node_data
+        nodes = np.array(list(self.brick_graph.nodes()))
+        l_out = nodes[nodes % 6 == 4]
+        l_in = nodes[nodes % 6 == 5]
 
-        R_identified_relabelled = nx.relabel_nodes(
-            R_identified, node_data(equivalent_nodes), copy=True
-        )
-        return R_identified_relabelled, self.id_to_muts
+        # Condensation creates graph with connected components (cc)
+        # we want mapping of cc to original nodes
+        cc_l_out = []
+        cc_l_in = []
+        mapping = C.graph["mapping"]
+        for out_node in l_out:
+            cc_l_out.append(mapping[out_node])
+        for out_node in l_in:
+            cc_l_in.append(mapping[out_node])
+        cc_l_out = np.array(cc_l_out)
+        cc_l_in = np.array(cc_l_in)
+
+        # Also correct the id_to_muts with mapping
+        mapped_id_to_muts = {}
+        for node_id in self.id_to_muts:
+            mapped_id_to_muts[node_id] = mapping[node_id * 6]
+
+        R = H.subgraph(np.concatenate([cc_l_in, cc_l_out]))
+        if self.identify_in_out:
+            nodes = list(R.nodes())
+            equivalent_nodes = []
+            for i in l_out:
+                equivalent_nodes.append((mapping[i], mapping[i + 1]))
+            print("Finding quotient graph...")
+            R_identified = nx.quotient_graph(R, equivalent_nodes)
+
+            def node_data(list_of_lists):
+                node_data = {}
+                for _, i in enumerate(list_of_lists):
+                    node_data[frozenset({i[0], i[1]})] = int(i[0] / 6)
+                return node_data
+
+            R_identified_relabelled = nx.relabel_nodes(
+                R_identified, node_data(equivalent_nodes), copy=True
+            )
+            return R_identified_relabelled, mapped_id_to_muts
+        else:
+            return R, mapped_id_to_muts
