a997f00e6cc841a098eeb08e98ab1c2ba84fce57
awohns <awohns@gmail.com>
2021-07-27 09:40:56 -0400

    add kwargs to regularisation

--- ld_graph/regularization.py
+++ ld_graph/regularization.py
@@ -44,41 +44,57 @@ class Regularize:
         tables.simplify(samples)
         return tables.tree_sequence()
 
-    def frequency_regularize(self, frequency):
-        edges = np.array(
-            [
-                (parent, child)
-                for child, parent in zip(
-                    self.ts.tables.edges.child, self.ts.tables.edges.parent
-                )
-            ]
-        )
-        G = nx.DiGraph()
-        G.add_edges_from(edges)
-        G_rev = G.reverse()
-        paths = np.zeros(G_rev.number_of_nodes())
-        for node in nx.topological_sort(G_rev):
-            if G.out_degree(node) == 0:
-                paths[node] = 1
-            else:
-                for edge in G.out_edges(node):
-                    paths[node] += paths[edge[1]]
-        keep_nodes = np.where(
-            np.isin(
-                np.arange(0, self.ts.num_nodes),
-                np.where(paths > (self.ts.num_samples * frequency))[0],
+
+def frequency_regularize(self, frequency, **kwargs):
+    """
+    Takes a tree sequence and removes nodes which are less than the given frequency.
+
+    :param TreeSequence tree_sequence: The input :class:`tskit.TreeSequence` to be
+        regularized.
+    :param float frequency: The maximum frequency of nodes to be retained in the tree
+        sequence. All nodes with frequency less than or equal to `frequency` will
+        be removed from the tree sequence.
+    :param \\**kwargs: All further keyword arguments are passed to the ``tskit.simplify``
+        The keyword argument `samples` cannot be passed.
+    :return: A tree sequence regularized by removing nodes based on their frequency.
+    :rtype: tskit.TreeSequence
+    """
+    if "samples" in kwargs:
+        raise ValueError("Cannot pass 'samples' to simplify()")
+    edges = np.array(
+        [
+            (parent, child)
+            for child, parent in zip(
+                self.ts.tables.edges.child, self.ts.tables.edges.parent
             )
-        )[0]
-        # Now we make a new tree sequence with only the keep_nodes
-        # to use ts.simplify(), we need to make all nodes samples
-        tables = self.ts.dump_tables()
-        tables.nodes.set_columns(
-            flags=np.ones(self.ts.num_nodes).astype("uint32"),
-            time=self.ts.tables.nodes.time,
+        ]
+    )
+    G = nx.DiGraph()
+    G.add_edges_from(edges)
+    G_rev = G.reverse()
+    paths = np.zeros(G_rev.number_of_nodes())
+    for node in nx.topological_sort(G_rev):
+        if G.out_degree(node) == 0:
+            paths[node] = 1
+        else:
+            for edge in G.out_edges(node):
+                paths[node] += paths[edge[1]]
+    keep_nodes = np.where(
+        np.isin(
+            np.arange(0, self.ts.num_nodes),
+            np.where(paths > (self.ts.num_samples * frequency))[0],
         )
-        all_samples_ts = tables.tree_sequence()
-        pruned_ts = all_samples_ts.simplify(samples=keep_nodes)
-        assert pruned_ts.num_nodes == len(
-            keep_nodes
-        )  # make sure we have right number of nodes
-        return pruned_ts
+    )[0]
+    # Now we make a new tree sequence with only the keep_nodes
+    # to use ts.simplify(), we need to make all nodes samples
+    tables = self.ts.dump_tables()
+    tables.nodes.set_columns(
+        flags=np.ones(self.ts.num_nodes).astype("uint32"),
+        time=self.ts.tables.nodes.time,
+    )
+    all_samples_ts = tables.tree_sequence()
+    pruned_ts = all_samples_ts.simplify(samples=keep_nodes, **kwargs)
+    assert pruned_ts.num_nodes == len(
+        keep_nodes
+    )  # make sure we have right number of nodes
+    return pruned_ts
