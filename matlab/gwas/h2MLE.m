4346d08f77cc92e76c3438594569c5f3c3292411
Luke Jen O'Connor <loconnor@broadinstitute.org>
2021-12-17 07:48:47 -0500

    h2MLE now computes standard errors and Z scores

--- matlab/gwas/h2MLE.m
+++ matlab/gwas/h2MLE.m
@@ -1,4 +1,4 @@
-function [params,newObjVal,allSteps,allValues,allStepSizes] = h2MLE(alphahat,P,varargin)
+function [estimate, steps] = h2MLE(alphahat,P,varargin)
 %h2MLE computes maximum-likelihood heritability estimates using gradient
 %descent and LDGMs
 %   Inputs:
@@ -40,6 +40,7 @@ addOptional(p, 'convergenceTol', 1e-6, @isscalar)
 addOptional(p, 'maxReps', 1e4, @isscalar)
 addOptional(p, 'minReps', 2, @isscalar)
 addOptional(p, 'method', 'gradient', @isstr)
+addOptional(p, 'noVarianceEstimate', 0, @isscalar)
 
 parse(p, alphahat, P, varargin{:});
 
@@ -75,8 +76,8 @@ end
 if 1 %strcmp(method,'coordinate') || strcmp(method,'weighted_gradient')
     
     stepSize = [1./sum(annot{1})'.^2/nn; ones(noParams-noAnnot,1)];
-%         stepSize = ones(noParams,1)/nn/mm^2;
-
+    %         stepSize = ones(noParams,1)/nn/mm^2;
+    
 elseif strcmp(method,'gradient')
     warning('This method seems to work poorly')
     stepSize = ones(noParams,1)/nn/mm^2;
@@ -85,7 +86,11 @@ else
 end
 % dampening = 0;
 
-allSteps=zeros(maxReps,noParams);allValues=zeros(maxReps,1);
+allSteps=zeros(min(maxReps,1e6),noParams);
+allValues=zeros(min(maxReps,1e6),1);
+allStepSizes=allSteps;
+allGradients=allSteps;
+
 for rep=1:maxReps
     
     oldGradient=gradient;
@@ -132,11 +137,12 @@ for rep=1:maxReps
     [params, stepSize(whichParam), newObjVal] = linesearch(params, ...
         oldObjVal, gradient, objFn, stepSize(whichParam), stepsize_factor);
     
-    allSteps(rep,:)=params;
     allValues(rep)=newObjVal;
-    allStepSizes(rep,:) = stepSize;
-    
-    
+    if nargout > 1
+        allSteps(rep,:)=params;
+        allStepSizes(rep,:) = stepSize;
+        allGradients(rep,:) = gradient;
+    end
     if rep > minReps
         if allValues(rep-minReps) - newObjVal < convergenceTol * minReps
             break;
@@ -144,6 +150,81 @@ for rep=1:maxReps
     end
     
     %     converged = (newObjVal-oldObjVal)/oldObjVal < convergenceTol;
+end
+
+% Convergence report
+if nargout > 1
+    steps.reps = rep;
+    steps.params = allSteps(1:rep,:);
+    steps.obj = allValues(1:rep);
+    steps.stepsize = allStepSizes(1:rep,:);
+    steps.gradient = allGradients(1:rep,:);
+end
+
+% From paramaters to h2 estimates
+h2Est = 0;annotSize=0;totalh2=0;
+for block=1:noBlocks
+    perSNPh2 = linkFn(annot{block}, params);
+    h2Est = h2Est + sum(perSNPh2.*annot{block});
+    annotSize = annotSize + sum(annot{block});
+end
+
+estimate.params = params;
+estimate.h2 = h2Est;
+estimate.annotSum = annotSize;
+
+% Enrichment only calculated if first annotation is all-ones vector
+if all(cellfun(@(a)all(a(:,1)==1),annot))
+    estimate.enrichment = (h2Est./annotSize) / (h2Est(1)/annotSize(1));
+end
+
+% Compute covariance of the parameter estimates
+if ~noVarianceEstimate
+    smallNumber = 1e-6;
+    gradient = 0;
+    for block = 1:noBlocks
+        sigmasq = linkFn(annot{block}, params);
+        sigmasqGrad = linkFnGrad(annot{block}, params);
+        gradient = gradient + ...
+            GWASlikelihoodGradient(alphahat{block},1./sigmasq,P{block},nn,sigmasqGrad,whichSNPs{block})';
+    end
+    
+    FI = zeros(noParams);
+    for k = 1:noParams
+        newParams = params;
+        newParams(k) = params(k) * (1+smallNumber);
+        gradient_k = 0;
+        for block = 1:noBlocks
+            sigmasq = linkFn(annot{block}, newParams);
+            sigmasqGrad = linkFnGrad(annot{block}, newParams);
+            gradient_k = gradient_k + ...
+                GWASlikelihoodGradient(alphahat{block},1./sigmasq,P{block},nn,sigmasqGrad,whichSNPs{block})';
+        end
+        
+        FI(:,k) = (gradient_k - gradient) / (smallNumber * params(k));
+    end
+    
+    % Variance of h2 estimates
+    dh2da = 0;
+    for block=1:noBlocks
+        dh2da = dh2da + linkFnGrad(annot{block}, params)'*annot{block};
+        
+    end
+    h2Var = dh2da'/FI * dh2da;
+    
+    
+    
+    estimate.paramsVar = pinv(FI);
+    estimate.FI = FI;
+    estimate.h2Var = h2Var;
+    
+    if all(cellfun(@(a)all(a(:,1)==1),annot))
+        estimate.enrichmentZscore = (h2Est./annotSize - h2Est(1)/annotSize(1)) ./ ...
+            sqrt(h2Var(1,:)./annotSize.^2 + h2Var(1)./annotSize(1).^2 - 2*h2Var(1,:)*h2Var(1)/annotSize(1)./annotSize);
+        estimate.enrichmentZscore(1) = 0;
+    end
+    
+    
 end
 
 end
