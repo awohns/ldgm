b1f755de636e68efab57c22a29c02ce333a10d3f
Luke Jen O'Connor <loconnor@broadinstitute.org>
2021-12-20 10:16:38 -0500

    Fix BLUP.m

--- matlab/gwas/BLUP.m
+++ matlab/gwas/BLUP.m
@@ -1,4 +1,4 @@
-function [betaExpectation] =...
+function [betaExpectation, h2MLE] =...
     BLUP(alphahat, P, nn, tau, whichSNPs)
 % BLUP computes the best linear unbiased predictor, E(beta|GWAS, gaussian
 % prior) where the GWAS data is alphaHat, the LD precision matrix is P, and
@@ -7,20 +7,29 @@ function [betaExpectation] =...
 % alphahat: GWAS sumstats; omega: LD precision matrix; nn: GWAS sample
 % size; tau: precision of beta; whichSNPs: rows/cols of P for which we have
 % data alphaHat
+% h2MLE: vector of MLE values for diag(beta*alpha'). Can be negative. Add
+% these up to get an MLE heritability estimate for a set of SNPs.
 
 mm = length(P);
 if isvector(tau)
-    tauMatrix = zeros(mm,1);
-    tauMatrix(whichSNPs) = tau;
-    tauMatrix = diag(sparse(tau));
+    sigmasq = zeros(mm,1);
+    sigmasq(whichSNPs) = 1./tau;
+    sigmasq = diag(sparse(sigmasq));
 end
 betahat = zeros(mm,1);
 betahat(whichSNPs) = P(whichSNPs,whichSNPs) * alphahat - ...
     P(whichSNPs,~whichSNPs) * (P(~whichSNPs,~whichSNPs) \ ...
     (P(~whichSNPs,whichSNPs)*alphahat));
 
-% betaExpectation = (tau + nn*inv(omega)) \ (nn * alphaHat);
-betaExpectation = (P*tauMatrix + nn*speye(mm)) \ (nn * betahat);
+% betaExpectation = (tauMatrix + nn*inv(P)) \ (nn * alphaHat);
+betaExpectation = sigmasq * ((P + nn*sigmasq) \ (nn * betahat));
+
+if nargout > 1
+    betaExpectation(~whichSNPs) = 0;
+    alphaExpectation = P \ betaExpectation;
+    h2MLE = betaExpectation(whichSNPs) .* alphaExpectation(whichSNPs);
+end
+
 betaExpectation = betaExpectation(whichSNPs);
 
 end
