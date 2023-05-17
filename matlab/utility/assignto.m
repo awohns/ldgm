4ea1e9cf05e5d8f791cebbb00700d91bc8836692
Luke Jen O'Connor <loconnor@broadinstitute.org>
2023-05-17 10:30:00 -0400

    Update assignto.m
    Handle empty input

--- matlab/utility/assignto.m
+++ matlab/utility/assignto.m
@@ -1,10 +1,21 @@
 
 function y = assignto(x, lgc, n)
+% Assign values in x to the nonzeros of lgc. lgc can be specified
+% instead as a list of indices, in which case third input argument sets
+% the number of rows of output y.
+if isempty(lgc)
+    assert(isempty(x))
+    if exist('n')
+        y = zeros(n,1);
+    else
+        y = [];
+    end
+else
     if ~islogical(lgc)
         lgc = unfind(lgc,n);
     end
-    % Assign values in x to the nonzeros of lgc
     assert(sum(lgc) == length(x))
-    y = zeros(size(lgc));
-    y(lgc) = x;
+    y = zeros(size(lgc,1), size(x,2));
+    y(lgc,:) = x;
+end
 end
